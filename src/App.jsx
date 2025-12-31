76cf9d2414bf835419dc7022ea8a1cf007856d83
Abhradip Seth <abhradipsethofficial06@gmail.com>
2025-12-31 22:48:26 +0530

    Some Bugs Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import UiContextProvider from "./Context/UiContext"
-import Navbar from "./Navbar"
-import SearchBox from "./SearchBox"
-import SearchFooter from "./SearchFooter"
-import SearchPageHeader from "./SearchPageHeader"
-
+import { useContext } from "react";
+import UiContextProvider, { UIContext } from "./Context/UiContext";
+import Navbar from "./Navbar";
+import SearchBox from "./SearchBox";
+import SearchFooter from "./SearchFooter";
+import SearchPageHeader from "./SearchPageHeader";
+import SearchResult from "./SearchResult";
 
 function App() {
-  
+  const { isSearchClicked } = useContext(UIContext);
 
   return (
     <>
       <div className="bg-[#f5f6f7] h-screen w-screen">
-       <Navbar/ >
-       <UiContextProvider>
-            <SearchPageHeader/>
-            <SearchBox />
-            <SearchFooter/>
-       </UiContextProvider>
+        <Navbar />
+
+        <SearchPageHeader />
+        <SearchBox />
+        {isSearchClicked ? <SearchResult /> : <SearchFooter />}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
